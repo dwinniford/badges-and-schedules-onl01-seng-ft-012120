fe3f74a1686f4552f87a3424761b043ce4534ae8
Dave Winniford <dwinniford@gmail.com>
2020-01-22 20:57:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,7 @@ end
 def batch_badge_creator(attendees)
   attendees.map { |person| badge_maker(person) }
 end 
+
+def assign_rooms(attendees)
+  attendees.map { |person| "Hello, #{person}! You'll be assigned to room #{person.index + 1}"
+end
